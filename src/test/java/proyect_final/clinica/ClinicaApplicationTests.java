acf5e9bea672be0984a246bf4fc0f26e35bf826d
Oscar562025 <oscarmujica2006@gmail.com>
2026-06-15 01:56:35 -0400

    Primer commit: Proyecto Clinica en Spring Boot

---
+++ src/test/java/proyect_final/clinica/ClinicaApplicationTests.java
@@ -0,0 +1,13 @@
+package proyect_final.clinica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
